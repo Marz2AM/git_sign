5f15e929501d63bc9b5dee3d75233ba65d9e2456
Marz2AM <flrudghks78@gmail.com>
2024-03-19 20:08:19 +0900

    홈과회원가입 구현

--- app/src/main/java/com/example/mysign/SignInActivity.kt
+++ app/src/main/java/com/example/mysign/SignInActivity.kt
@@ -1,11 +1,42 @@
 package com.example.mysign
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val paTh1 = findViewById<EditText>(R.id.path_id)
+        val paTh2 = findViewById<EditText>(R.id.path_pt)
+        val loginBtn = findViewById<Button>(R.id.btn_login)
+        val signBtn = findViewById<Button>(R.id.btn_sign)
+
+        loginBtn.setOnClickListener {
+            if(paTh1.text.isEmpty()){
+                Toast.makeText(this,"아이디/비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            if (paTh2.text.isEmpty()){
+                Toast.makeText(this,"아이디/비밀번호를 확인해주세요", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            val intent = Intent(this@SignInActivity, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        signBtn.setOnClickListener {
+            val intent2 = Intent(this@SignInActivity, SignUpActivity::class.java)
+            startActivity(intent2)
+        }
+
+
+
+
     }
 }
